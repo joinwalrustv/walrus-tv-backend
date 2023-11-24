81fac70d34b5d19b9c704f8eace1fe350e61b181
Nitin Ankad <46038298+nitinankad@users.noreply.github.com>
2023-11-24 14:10:23 -0600

    Add server tests

---
+++ test/server.test.ts
@@ -0,0 +1,17 @@
+import chai from "chai";
+import chaiHttp from "chai-http";
+import "mocha";
+import server from "../src/server";
+
+chai.use(chaiHttp);
+
+const expect = chai.expect;
+const request = chai.request;
+
+describe("GET /abcdef", () => {
+  it("should not be found", async () => {
+      const res = await request(server).get("/abcdef")
+      expect(res.body.message).to.equal("not found");
+      expect(res.status).to.equal(404);
+  });
+});
