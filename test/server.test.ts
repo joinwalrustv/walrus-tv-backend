af04074e5063227209114eb9a6c60a1df80974a9
Nitin Ankad <46038298+nitinankad@users.noreply.github.com>
2023-11-24 23:08:31 -0600

    Add more unit tests for Server

--- test/server.test.ts
+++ test/server.test.ts
@@ -2,11 +2,44 @@ import chai from "chai";
 import chaiHttp from "chai-http";
 import "mocha";
 import server from "../src/server";
+import sinon from "sinon";
+import http from "http";
 
 chai.use(chaiHttp);
 
 const expect = chai.expect;
 const request = chai.request;
+
+describe("Server", () => {
+  let listenStub: sinon.SinonStub;
+
+  const loadServer = () => {
+    delete require.cache[require.resolve("../src/server")]; // Adjust the path as necessary
+    return require("../src/server");
+  };
+
+  beforeEach(() => {
+      listenStub = sinon.stub(http.Server.prototype, "listen");
+  });
+
+  afterEach(() => {
+      listenStub.restore();
+  });
+
+  it("should start on env port", () => {
+      process.env.PORT = "5000";
+      loadServer();
+
+      expect(listenStub.calledWith("5000")).to.be.true;
+  });
+
+  it("should default to port 8000", () => {
+      delete process.env.PORT;
+      loadServer();
+
+      expect(listenStub.calledWith(8000)).to.be.true;
+  });
+});
 
 describe("GET /abcdef", () => {
   it("should not be found", async () => {
