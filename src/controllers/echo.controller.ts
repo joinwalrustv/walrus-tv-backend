abd63f6e4fbcf07922183326c28b922bf3cb689d
Nitin Ankad <46038298+nitinankad@users.noreply.github.com>
2024-01-17 22:41:33 -0600

    Fix indentation in echo controller

--- src/controllers/echo.controller.ts
+++ src/controllers/echo.controller.ts
@@ -1,9 +1,9 @@
 import { Request, Response, NextFunction } from "express";
 
 export const getEchoMessage = async (req: Request, res: Response, next: NextFunction) => {
-    let message: String = "Walrus TV";
+  let message: String = "Walrus TV";
 
-    return res.status(200).json({
-        message: message
-    });
+  return res.status(200).json({
+    message: message
+  });
 };
